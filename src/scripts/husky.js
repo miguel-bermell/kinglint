122782b429e7d8216b30a99a7d1333714039153c
miguel-bermell <miguelbermell@gmail.com>
2023-02-25 17:59:44 +0100

    fix: husky script

--- src/scripts/husky.js
+++ src/scripts/husky.js
@@ -1,3 +1,3 @@
 export const huskyScript = {
-	"prepare:husky": `npm i -D husky lint-staged && husky install && npx husky add .husky/pre-commit "npx lint-staged" && npx husky add .husky/commit-msg ""`,
+	"prepare:husky": `husky install && npx husky add .husky/pre-commit "npx lint-staged" && npx husky add .husky/commit-msg ""`,
 };
